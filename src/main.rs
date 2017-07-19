e6f498da9769a897dd3c30e404637bc967703655
JD Gonzales <jd_gonzales@icloud.com>
2017-07-18 20:23:50 -0500

    Implemented JSON serialization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate serde;
+extern crate serde_json;
+
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde_derive;
+
+use rocket_contrib::Json;
+
+#[derive(Serialize, Deserialize)]
 #[derive(Debug)]
 struct Movie {
+    id: i32,
     title: String,
     rating: String,
     director: String,
@@ -7,17 +23,28 @@ struct Movie {
 }
 
 impl Movie {
-    fn new () -> Movie {
+    fn new (id: i32, title: String, rating: String, director: String, score: i32) -> Movie {
         Movie {
-            title: String::from("The Matrix"),
-            rating: String::from("R"),
-            director: String::from("The Wachowskis"),
-            score: 87
+            id: id,
+            title: String::from(title),
+            rating: String::from(rating),
+            director: String::from(director),
+            score: score
         }
     }
 }
 
+#[get("/movies")]
+fn movie() -> Json<Movie> {
+    let title = String::from("The Matrix");
+    let rating = String::from("R");
+    let director = String::from("The Wachowski's");
+
+    Json(Movie::new(1, title, rating, director, 87))
+}
+
 fn main() {
-    let movie = Movie::new();
-    println!("{:?}", movie);
+    rocket::ignite()
+        .mount("/", routes![movie])
+        .launch();
 }
